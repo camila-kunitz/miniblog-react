c0c9bfaa2b03d5a08bb90c773d5ab6dc8ec72eff
Camila Kunitz <camila.kunitz@gmail.com>
2022-05-23 10:52:27 -0300

    Integra Firebase ao React

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-2PC1WxwP2eTPzBJ-t8RvrCdY3AMsy6k',
+  authDomain: 'miniblog-df746.firebaseapp.com',
+  projectId: 'miniblog-df746',
+  storageBucket: 'miniblog-df746.appspot.com',
+  messagingSenderId: '160025757683',
+  appId: '1:160025757683:web:f72eb7813831a8d16b7486',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
