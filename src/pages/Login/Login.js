cf022ee4b7f01580ab667b2409c3dbd2df98fc49
Camila Kunitz <camila.kunitz@gmail.com>
2022-05-26 10:05:47 -0300

    Estrutura página de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,72 @@
+import { useEffect, useState } from 'react';
+import { useAuthentication } from '../../hooks/useAuthentication';
+
 // CSS
 import styles from './Login.module.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setError('');
+
+    const user = {
+      email,
+      password,
+    };
+
+    const res = await createUser(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className={styles.login}>
+      <h1>Entrar</h1>
+      <p>Faça o login para poder utilizar o sistema</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        {!loading && <button className="btn">Entrar</button>}
+        {loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className="error">{error}</p>}
+      </form>
     </div>
   );
 };
